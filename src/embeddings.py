bd3778e499a65cc90c5ed6e1f092c1c080cf9db1
Hemant Dandotiya <hemantd369@gmail.com>
2026-05-29 01:06:45 +0530

    Main Branch For RAG Overview

--- src/embeddings.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
-
-def get_embeddings_models():
-   
-   print("Loading HuggingFace Embeddings model...")
-   embeddings_models = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
-   print("HuggingFace Embeddings model loaded.")
-
-   return embeddings_models
